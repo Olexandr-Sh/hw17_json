8e6e31805760a9519cfcad8965c5f31311da5cc0
Olexandr-Sh <shevtcov07@ukr.net>
2021-05-27 23:37:32 +0300

    replacer json

--- json_hw17.js
+++ json_hw17.js
@@ -7,4 +7,22 @@ let car = {
 };
 
 let carJson = JSON.stringify(car);
-console.log(typeof carJson);
+console.log(typeof carJson, carJson);
+
+let idNumber = {
+  number: 35,
+};
+
+let choise = {
+  car: 'Mazda 6',
+  models: [{ model: 'Grand touring' }, { model: 'Sport' }],
+  code: idNumber,
+};
+
+idNumber.occupiedBy = choise;
+
+alert( JSON.stringify(choise, function replacer(key, value) {
+  alert(`${key}: ${value}`);
+  return (key == 'occupiedBy') ? undefined : value;
+}));
+
