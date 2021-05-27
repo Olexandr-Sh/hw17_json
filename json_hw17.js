23c4e4d2efbdb75451d07aa6e1d079660cbff194
Olexandr-Sh <shevtcov07@ukr.net>
2021-05-27 23:12:20 +0300

    practise - json stringify

---
+++ json_hw17.js
@@ -0,0 +1,10 @@
+let car = {
+  brand: 'ford fusion',
+  age: 5,
+  engine: 2.5,
+  color: 'burgundy',
+  graduationYear: 2013,
+};
+
+let carJson = JSON.stringify(car);
+console.log(typeof carJson);
